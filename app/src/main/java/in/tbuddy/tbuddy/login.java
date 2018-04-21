6cf6179563345d5cb049326169d2fbaa5efff153
Vysakh Murali <contactvyshu@gmail.com>
2018-04-21 23:47:16 +0530

    #6 (Register/Account Type) Layout Completed.

--- app/src/main/java/in/tbuddy/tbuddy/login.java
+++ app/src/main/java/in/tbuddy/tbuddy/login.java
@@ -1,7 +1,9 @@
 package in.tbuddy.tbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.regtotype).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(login.this,studortea.class);
+                startActivity(a);
+            }
+        });
     }
 
 
