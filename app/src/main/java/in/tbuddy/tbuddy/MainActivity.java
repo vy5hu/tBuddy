e39e107f8f2153b5f43fa902a6f4490ffe5b72ac
Vysakh Murali <contactvyshu@gmail.com>
2018-04-21 12:03:08 +0530

    #3 Start Screen Ripple Changed From Button to Layout

--- app/src/main/java/in/tbuddy/tbuddy/MainActivity.java
+++ app/src/main/java/in/tbuddy/tbuddy/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Animation frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
-        findViewById(R.id.button_a).setAnimation(frombottom);
+//        Animation frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+//        findViewById(R.id.button_a).setAnimation(frombottom);
     }
 }
