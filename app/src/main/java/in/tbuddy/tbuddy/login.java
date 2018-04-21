8e13f0a69ee1177deed9190e36568f7821cbda27
Vysakh Murali <contactvyshu@gmail.com>
2018-04-22 01:29:22 +0530

    #7 Layout Slide Animation Added

--- app/src/main/java/in/tbuddy/tbuddy/login.java
+++ app/src/main/java/in/tbuddy/tbuddy/login.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class login extends AppCompatActivity {
 
@@ -16,6 +19,7 @@ public class login extends AppCompatActivity {
             public void onClick(View v) {
                 Intent a=new Intent(login.this,studortea.class);
                 startActivity(a);
+                overridePendingTransition(R.anim.slidein_right, R.anim.slideout_left);
             }
         });
     }
