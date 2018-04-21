e0050e7419fd6f2fa614e1ccd40066a230812ff2
Vysakh Murali <contactvyshu@gmail.com>
2018-04-21 10:34:03 +0530

    #2 Added Animation & Ripple Effect to Button

--- app/src/main/java/in/tbuddy/tbuddy/MainActivity.java
+++ app/src/main/java/in/tbuddy/tbuddy/MainActivity.java
@@ -2,6 +2,8 @@ package in.tbuddy.tbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Animation frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+        findViewById(R.id.button_a).setAnimation(frombottom);
     }
 }
