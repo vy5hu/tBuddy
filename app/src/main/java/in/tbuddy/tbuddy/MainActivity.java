e196300277b012cd9b58e8683d5e135e049ada30
Vysakh Murali <contactvyshu@gmail.com>
2018-04-21 12:24:55 +0530

    #4 Start Screen Gradient Minor Changes & New Login Activity Created & Connected

--- app/src/main/java/in/tbuddy/tbuddy/MainActivity.java
+++ app/src/main/java/in/tbuddy/tbuddy/MainActivity.java
@@ -1,7 +1,9 @@
 package in.tbuddy.tbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -12,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Animation frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
-//        findViewById(R.id.button_a).setAnimation(frombottom);
+        findViewById(R.id.to_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logscr= new Intent(MainActivity.this,login.class);
+                startActivity(logscr);
+            }
+        });
     }
 }
